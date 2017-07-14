fa66e4dff078feaa36d05ee32cd59798932ed9d2
wuxinIOS <wuxinIOS@163.com>
2017-07-14 15:32:30 +0800

    新建手势交互管理类WXPercentInteractiveTransition

--- WXTransitionDemo/MagicMove/WXMagicMovePushController.m
+++ WXTransitionDemo/MagicMove/WXMagicMovePushController.m
@@ -36,6 +36,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+
 //动画效果
 - (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC{
     
@@ -51,14 +52,5 @@
 }
 
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
---
+++ WXTransitionDemo/WXInteractiveTransition/WXPercentInteractiveTransition.m
@@ -0,0 +1,13 @@
+//
+//  WXPercentInteractiveTransition.m
+//  WXTransitionDemo
+//
+//  Created by BlackEr Gray on 17/7/14.
+//  Copyright © 2017年 BlackEr Gray. All rights reserved.
+//
+
+#import "WXPercentInteractiveTransition.h"
+
+@implementation WXPercentInteractiveTransition
+
+@end
